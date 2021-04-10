4b4bd7b3e1fbf74c93c382f6657260b298932d56
Omar Monteon <monteon.omar@gmail.com>
2021-04-09 20:54:26 -0500

    feat: Add stack navigator. Update naming.

--- components/App.js
+++ components/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-function HomeScreen() {
+function DetailsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Detalles</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
+function DecksScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Decks screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
+  );
+}
+
+function AddDeckScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Add Deck Screen</Text>
+    </View>
+  );
+}
+
+const DecksStack = createStackNavigator();
+
+function DecksStackScreen() {
+  return (
+    <DecksStack.Navigator>
+      <DecksStack.Screen name="Decks" component={DecksScreen} />
+      <DecksStack.Screen name="Details" component={DetailsScreen} />
+    </DecksStack.Navigator>
+  );
+}
+
+const AddDeckStack = createStackNavigator();
+
+function AddDeckStackScreen() {
+  return (
+    <AddDeckStack.Navigator>
+      <AddDeckStack.Screen name="Add deck" component={AddDeckScreen} />
+      <AddDeckStack.Screen name="Details" component={DetailsScreen} />
+    </AddDeckStack.Navigator>
   );
 }
 
@@ -26,8 +61,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Decks" component={DecksStackScreen} />
+        <Tab.Screen name="Add Deck" component={AddDeckStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
