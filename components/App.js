8d0fbe39c42ac4511c6931b87de6dd5372192df3
Omar Monteon <monteon.omar@gmail.com>
2021-04-10 19:25:39 -0500

    feat: Wire DeckScreen in the router.

--- components/App.js
+++ components/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, AsyncStorage, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import DecksListScreen from './DeckListScreen';
 import AddDeckScreen from './AddDeckScreen';
+import DeckScreen from './DeckScreen';
 import Constants from 'expo-constants';
-import { useEffect } from 'react/cjs/react.development';
 
-function UdaciStatusBar ({backgroundColor, ...props}) {
+function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
     <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
@@ -31,7 +31,10 @@ function DecksStackScreen() {
   return (
     <DecksStack.Navigator>
       <DecksStack.Screen name="Decks" component={DecksListScreen} />
-      <DecksStack.Screen name="Details" component={DetailsScreen} />
+      <DecksStack.Screen
+        name="DeckDetails"
+        component={DeckScreen}
+        options={({ route }) => ({ title: route.params.deck.title })} />
     </DecksStack.Navigator>
   );
 }
