562d7c59e9b9b6287c1bb33d9c6a6f61fb5e29e5
Omar Monteon <monteon.omar@gmail.com>
2021-04-10 21:01:32 -0500

    feat: Add more functions to API:
    - fecthDeckFromStorage
    - addCardToStorage
    
    fix: generating two different ids.

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ import { AsyncStorage } from 'react-native';
 
 // Helper function to generate a pseudo unique id.
 // Code taken from: https://stackoverflow.com/a/53116778
-const uid = function(){
+const uid = function () {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
@@ -10,41 +10,52 @@ export function fetchDecksFromStorage() {
   return AsyncStorage.getItem('decks');
 }
 
+export async function fetchDeckFromStorage(id) {
+  const decksString = await AsyncStorage.getItem('decks');
+  try {
+    const decks = JSON.parse(decksString);
+    return decks?.[id];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function addDeckToStorage(deck) {
-  const decksString = await fetchDecksFromStorage(); 
+  const decksString = await fetchDecksFromStorage();
   try {
     let decks = {};
+    const id = uid();
     if (decksString) {
       decks = JSON.parse(decksString);
-    }    
-    decks[`id_${uid()}`] = deck;
+    }
+    decks[`id_${id}`] = {
+      id: `id_${id}`,
+      ...deck
+    };
     return AsyncStorage.setItem('decks', JSON.stringify(decks));
   } catch (error) {
     console.error(error);
   }
 }
 
-// {
-//   React: {
-//     title: 'React',
-//     questions: [
-//       {
-//         question: 'What is React?',
-//         answer: 'A library for managing user interfaces'
-//       },
-//       {
-//         question: 'Where do you make Ajax requests in React?',
-//         answer: 'The componentDidMount lifecycle event'
-//       }
-//     ]
-//   },
-//   JavaScript: {
-//     title: 'JavaScript',
-//     questions: [
-//       {
-//         question: 'What is a closure?',
-//         answer: 'The combination of a function and the lexical environment within which that function was declared.'
-//       }
-//     ]
-//   }
-// }
+export async function addCardToStorage(deckId, question) {
+  const decksString = await fetchDecksFromStorage();
+  try {
+    let decks = {};
+    if (decksString) {
+      decks = JSON.parse(decksString);
+      console.log('API | Deck id:', deckId);
+      decks[deckId] = {
+        ...decks[deckId],
+        questions: decks[deckId] 
+        ? decks[deckId].questions.concat(question) 
+        : [question]
+      };
+    } else {
+      console.log('There are no decks in the storage');
+    }
+    return AsyncStorage.setItem('decks', JSON.stringify(decks));
+  } catch (error) {
+    console.error(error);
+  }
+}
